d72faac7f45ca17f7d7f5d76a9694b2f5c37ee1a
fortunatus <ihedfortune@gmail.com>
2024-01-27 22:44:03 +0100

    setup router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
 import "./App.css";
 import SideNav from "./components/Navigation/SideNav";
 import BasicCalendar from "./components/basicCalendat/BasicCalendar";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import { Calendar } from "react-big-calendar";
+import Events from "./pages/Events";
+
+const Layout = () => {
+  return (
+    <div>
+     <SideNav />
+      <ScrollRestoration />
+      <Outlet />
+      
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout />,
+    children:[
+      {
+        path:"/",
+        element: <Dashboard />,
+        // loader: productData,
+      },
+      {
+        path:"/calendar",
+        element: <BasicCalendar />
+      },
+      {
+        path:"/events",
+        element: <Events />
+      },
+      
+    ]
+  }
+])
+
 
 function App() {
   return (
     <div className="app">
       <div className="content">
-        <SideNav />
+        
         <div className="calendar-container">
-          <BasicCalendar />
+        <RouterProvider router={router}/>
         </div>
       </div>
     </div>
