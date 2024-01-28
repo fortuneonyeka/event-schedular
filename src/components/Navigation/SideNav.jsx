bba1268238b193cb848d21e188e45195f9fa729c
fortunatus <ihedfortune@gmail.com>
2024-01-28 21:24:35 +0100

    fix nav responsiveness

--- src/components/Navigation/SideNav.jsx
+++ src/components/Navigation/SideNav.jsx
@@ -1,38 +1,39 @@
-import React from 'react';
-import "./sidenav.css";
+import React, { useState } from 'react';
+import './sidenav.css';
 
 const SideNav = () => {
-  const navItems =  [
-    {
-      navItem: "Dashboard",
-      id: 1,
-      url: "/"
-    },
-    {
-      navItem: "Calendar",
-      id: 2, // Updated ID for Calendar
-      url: "/calendar"
-    },
-    {
-      navItem: "Events",
-      id: 3, // Updated ID for Events
-      url: "/events"
-    },
-    
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // Define array of nav items
+  const navItems = [
+    { text: 'Dashboard', link: '/' , id: 1},
+    { text: 'Events', link: '/events' , id: 2},
+    { text: 'Calendar', link: '/calendar', id: 3 }
   ];
 
   return (
-    <div className='container'>
-      <div className='sidenav'>
-        <ul className='navItems'>
-          {navItems.map((item) => (
-            <li key={item.id} className={window.location.pathname === item.url ? 'active' : ''}>
-              <a href={item.url}>{item.navItem}</a>
-            </li>
-          ))}
-        </ul>
+    <nav className={`navbar ${isOpen ? 'open' : ''}`}>
+      <div className="nav-header">
+        <h3>My App</h3>
+        <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleNav}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
       </div>
-    </div>
+      {/* Map over navItems to generate list */}
+      <ul className={`nav-items ${isOpen ? 'open' : ''}`}>
+        {navItems.map((item) => (
+          <li key={item.id} className={window.location.pathname === item.link ? 'active' : ''}>
+            <a href={item.link} onClick={toggleNav}>{item.text}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
